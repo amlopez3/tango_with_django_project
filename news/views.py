77be1cdb0a5f4b0d824aeaca3c1437800b674b2b
Amy Ceesay Lopez <amyangela3@hotmail.com>
2023-03-21 16:02:57 +0000

    data-driven view

--- news/views.py
+++ news/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from news.models import New
 
 def index(request):
+    news_list = New.objects.all()
+
     context_dict = {}
+
+    context_dict['news'] = news_list
+
     return render(request, 'news/index.html', context=context_dict)
 
